8b66f4034d5e3628dca663d9c1c22735a7e00a72
YusupovJavaDev <baxtiyorking17@gmail.com>
2025-10-01 21:40:45 +0500

    project is running

--- card/src/main/java/card/CardApplication.java
+++ card/src/main/java/card/CardApplication.java
@@ -2,12 +2,14 @@ package card;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CardApplication.class, args);
+    }
 
 }
